7cd9ea7a0517f01a241a3687780668971e71e0f1
d-maddy <satyabrata1112@gmail.com>
2023-05-08 16:23:13 +0530

    eleBytag

--- firstcommit.js
+++ firstcommit.js
@@ -9,17 +9,34 @@ console.log(document.title);
 // console.log(document.link);
 // console.log(document.images);
 
-let head=document.getElementById('header-title');
-console.log(head);
+// let head=document.getElementById('header-title');
+// console.log(head);
 
 // head.innerText='my own list'
 // head.textContent='my list'
 // head.style.borderBottom="solid black"
 // head.innerHTML="<h3>hello</h3>"
-let header=document.getElementById('main-header');
-header.style.borderBottom="solid black"
-let addI=document.getElementById('add')
-addI.style.color="green"
-addI.style.fontWeight="bold"
+// let header=document.getElementById('main-header');
+// header.style.borderBottom="solid black"
+// let addI=document.getElementById('add')
+// addI.style.color="green"
+// addI.style.fontWeight="bold"
 
+// let items=document.getElementsByClassName('list-group-item')
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent=" hello"
+// items[1].style.fontWeight="bold"
+// items[2].style.backgroundColor="green"
+
+// for(let i=0;i<items.length;i++){
+// items[i].style.fontWeight="bold"
+// }
+
+let tag=document.getElementsByTagName('li')
+console.log(tag);
+console.log(items[1]);
+tag[1].textContent=" hello"
+tag[1].style.fontWeight="bold"
+tag[2].style.backgroundColor="green"
 
