5a4037819df60a77896a9b80cef09a2e3f4a2142
d-maddy <satyabrata1112@gmail.com>
2023-05-08 17:41:45 +0530

    query

--- firstcommit.js
+++ firstcommit.js
@@ -1,4 +1,4 @@
-console.log(document.title);
+//console.log(document.title);
 // console.log(document.URL);
 // console.log(document.doctype);
 // console.log(document.head);
@@ -33,10 +33,52 @@ console.log(document.title);
 // items[i].style.fontWeight="bold"
 // }
 
-let tag=document.getElementsByTagName('li')
-console.log(tag);
-console.log(items[1]);
-tag[1].textContent=" hello"
-tag[1].style.fontWeight="bold"
-tag[2].style.backgroundColor="green"
+// let tag=document.getElementsByTagName('li')
+// console.log(tag);
+// console.log(items[1]);
+// tag[1].textContent=" hello"
+// tag[1].style.fontWeight="bold"
+// tag[2].style.backgroundColor="green"
+
+
+
+// let header=document.querySelector('#main-header');
+// header.style.borderBottom=' solid black 4px'
+
+// let input=document.querySelector('input');
+// input.value="hello world";
+
+// let submit=document.querySelector('input[type="submit"]');
+// submit.value="send";
+
+// let item =document.querySelector('.list-group-item');
+// item.style.color="red";
+
+// let lItem=document.querySelector('.list-group-item:last-child');
+// lItem.style.color='green'
+
+// let secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green'
+// let thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.color='white'
+
+
+// let title=document.querySelectorAll('.title')
+// console.log(title);
+
+// let item= document.querySelectorAll('.list-group-item:nth-child(2')
+// item.style.color='green'
+
+
+
+
+
+let odd=document.querySelectorAll('li:nth-child(odd)')
+let item= document.querySelectorAll('.list-group-item:nth-child(2')
+
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green"
+    item[i].style.color='green'
+}
+
 
