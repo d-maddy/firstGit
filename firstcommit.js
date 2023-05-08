8473882a714b21f033a5e19801530d930230817d
d-maddy <satyabrata1112@gmail.com>
2023-05-08 12:54:35 +0530

    domM

--- firstcommit.js
+++ firstcommit.js
@@ -1,1 +1,25 @@
-console.log('i love git')
+console.log(document.title);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.form);
+// console.log(document.link);
+// console.log(document.images);
+
+let head=document.getElementById('header-title');
+console.log(head);
+
+// head.innerText='my own list'
+// head.textContent='my list'
+// head.style.borderBottom="solid black"
+// head.innerHTML="<h3>hello</h3>"
+let header=document.getElementById('main-header');
+header.style.borderBottom="solid black"
+let addI=document.getElementById('add')
+addI.style.color="green"
+addI.style.fontWeight="bold"
+
+
