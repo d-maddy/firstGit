cf1db89351ed9a10a10d3716b5b53b69a41036f1
d-maddy <satyabrata1112@gmail.com>
2023-05-09 22:35:31 +0530

    deleteBtn

--- main.js
+++ main.js
@@ -7,60 +7,69 @@
 // ul.lastElementChild.innerHTML="<h1>Hello</h1>";
 // ul.children[0].style.color='green';
 // ul.children[1].style.color='yellow';
-const btn=document.querySelector('.btn');
-const myForn=document.querySelector('#my-form');
-const nameInput=document.querySelector('#name');
-const emailInput=document.querySelector('#email');
-const msg=document.querySelector('.msg');  
-const userList=document.querySelector('#users');
-const phNum=document.getElementById('phone');
+const btn = document.querySelector(".btn");
+const myForn = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
+const phNum = document.getElementById("phone");
 
-
-btn.addEventListener('mouseover',(e)=>{
-e.preventDefault();
-document.querySelector('body').classList.add('bg-dark');
+btn.addEventListener("mouseover", (e) => {
+  e.preventDefault();
+  document.querySelector("body").classList.add("bg-dark");
 });
 
-btn.addEventListener('mouseout',(e)=>{
-    e.preventDefault();
-    document.querySelector('body').classList.add('bg-dark');
-    });
+btn.addEventListener("mouseout", (e) => {
+  e.preventDefault();
+  document.querySelector("body").classList.add("bg-dark");
+});
 
-    myForn.addEventListener('submit',onsubmit);
-    function onsubmit(e){
-        e.preventDefault();
+myForn.addEventListener("submit", onsubmit);
+function onsubmit(e) {
+  e.preventDefault();
 
-        let obj={
-            name:nameInput.value,
-            email:emailInput.value,
-            phone:phNum.value
-        }
-        let user=JSON.stringify(obj);
-        localStorage.setItem(emailInput.value, user);
+  let obj = {
+    name: nameInput.value,
+    email: emailInput.value,
+    phone: phNum.value,
+  };
+  let user = JSON.stringify(obj);
+  localStorage.setItem(emailInput.value, user);
 
-        let objN=JSON.parse(localStorage.getItem('user'));
-       
-        
+  // let objN=JSON.parse(localStorage.getItem('user'));
+  // localStorage.setItem('name',nameInput.value)
+  // localStorage.setItem('email',emailInput.value)
+  // localStorage.setItem(nameInput.value,emailInput.value);
+  // console.log(nameInput.value);
+  // console.log(emailInput.value);
 
-        // localStorage.setItem('name',nameInput.value)
-        // localStorage.setItem('email',emailInput.value)
-        
-        // localStorage.setItem(nameInput.value,emailInput.value);
+  const li = document.createElement("li");
 
-        // console.log(nameInput.value);
-        // console.log(emailInput.value);
+  let deleteBtn = document.createElement("input");
+  deleteBtn.type = "button";
+  deleteBtn.value = "Delete";
 
-        if(nameInput.value==='' || emailInput.value===''){ 
-            msg.classList.add('error');
-            msg.innerHTML='please enter all the fields'; 
-            setTimeout(()=>msg.remove(),3000);
-        }
-        else{
-            const li=document.createElement('li');
-            li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value} :${phNum.value}`));
-            userList.appendChild(li);
-            nameInput.value='';
-            emailInput.value='';
-            phNum.value='';
-        }
-    }
+  deleteBtn.onclick = () => {
+    userList.removeChild(li);
+    localStorage.removeItem(obj.email);
+  };
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "please enter all the fields";
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    li.appendChild(
+      document.createTextNode(
+        `${nameInput.value} : ${emailInput.value} : ${phNum.value}`
+      )
+    );
+    li.appendChild(deleteBtn);
+    userList.appendChild(li);
+
+    nameInput.value = "";
+    emailInput.value = "";
+    phNum.value = "";
+  }
+}
